8e40710cb7abb98b194db32e934fbd885506a497
Britte <31494508+brittegenzlinger@users.noreply.github.com>
2025-09-16 15:00:17 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# lab_file_handling
+
+Practice exercises for file handling using readline() and while loops.
---
+++ show_inventory.py
@@ -0,0 +1,7 @@
+'''Write a function that takes in a file path as a parameter
+    and loops through each item
+    and then prints out only in in stock items in the format:
+    Laptop - $1200.00
+    Mouse - $25.00
+    ...
+'''
